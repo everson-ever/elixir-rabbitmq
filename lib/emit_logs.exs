0cce30ca3bd0d4dcc3ea23cc067758e1e7e19ac7
Everson Silva <eversonsilva9799@gmail.com>
2021-11-16 07:27:17 -0300

    feat: fanout exchange t3

--- lib/emit_logs.exs
+++ lib/emit_logs.exs
@@ -1,16 +1,14 @@
 {:ok, connection} = AMQP.Connection.open
 {:ok, channel} = AMQP.Channel.open(connection)
 
-queue = "reports_queue"
-AMQP.Queue.declare(channel, queue, durable: true)
-
 message =
   case System.argv do
     []    -> "Hello World!"
     words -> Enum.join(words, " ")
   end
 
+AMQP.Exchange.declare(channel, "logs", :fanout)
+AMQP.Basic.publish(channel, "logs", "", message)
+IO.puts " [x] Sent '#{message}'"
 
-AMQP.Basic.publish(channel, "", queue, message, persistent: true)
-IO.puts " [x] Sent #{message}"
 AMQP.Connection.close(connection)
